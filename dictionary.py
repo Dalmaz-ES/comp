878c8126e4a44ed54059a25713408d8578241c02
Dalmaz-ES <eyipsinay@gmail.com>
2026-08-17 20:49:23 +0200

    17/08

--- dictionary.py
+++ dictionary.py
@@ -1,19 +1,58 @@
 
 capitals = {"USA": "Washington",
             "UK": "London",
-            "Russia": "Moscow"}
-
+            "RUS": "Moscow"}
+"""
 print("capitals:")
 for x, y in capitals.items():
     print(x, y)
 print()
+"""
 
 
-#test
+#print(dir(capitals)) to see different attributes of capitals
+#print(help(capitals))
 
-#10/05/2025 continue on dict
-#test devam
-#ekstra deneme
-#son test
-#yeni test
+#TO FIND THE CAPITAL IF EXISTS IN DICTIONARY
+"""
+if capitals.get("USA"):
+    print("Capital is in the dictionary")
+else:
+    print("That is not in the dictionary")
+"""
+
+
+#TO UPDATE OR CHANGE THE DICTIONARY
+"""
+capitals.update({"TR": "Ankara"})
+print(capitals)
+capitals.pop("RUS") # removes the key and value pair
+capitals.popitem() # removes the last item
+capitals.clear() # removes all items
+capitals.keys() # returns the keys
+
+keys = capitals.keys() # returns the keys and iterates over them
+for key in capitals.keys():
+    print(key)
+    
+capitals.values() # returns the values
+
+for value in capitals.values(): # iterate over the values
+    print(value)
+    
+capitals.items() # returns the key and value pairs
+
+for key, value in capitals.items(): # iterate over the key and value pairs
+    print(key, value)
+"""
+
+
+#TO FIND THE CAPITAL WHEN ENTERED THE COUNTRY
+"""
+country = input("Enter the Country: ").upper()
+if country in capitals:
+    print(f"The capital is {capitals[country]}")
+else:
+    print("That capital does not exist")
+"""
 
--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+
+#MARKET EXAMPLE
+"""
 banana_price = 7.49
 apple_price = 5.95
 
@@ -13,3 +16,68 @@ elif item == "banana":
 
 else:
     print("Not a valid item")
+"""
+
+
+
+#QUIZ GAME TEST
+"""
+questions = ("WHATS MY NAME?: ",
+             "WHATS MY AGE?: ",
+             "WHATS M Y HEIGHT?: ",
+             "WHATS MY COUNTRY?: ")
+
+options = (("A.John", "B.Sinay", "C.Doe", "D.Smith"),
+           ("A.22", "B.23", "C.24", "D.25"),
+           ("A.5.7", "B.5.8", "C.5.9", "D.6.0"),
+           ("A.USA", "B.Canada", "C.UK", "D.Australia"))
+
+answers = ("B", "B", "C", "A")
+
+guesses = []
+
+score = 0
+question_num = 0
+
+
+for x in questions:
+    print("-------------------------")
+    print(x)
+
+    for y in options[question_num]:
+        print(y)
+
+
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess) # adds the guess to the guesses list
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT!")
+
+    else:
+        print("WRONG!")
+        print(f"{answers[question_num]}. is the correct answer")
+    question_num += 1
+
+print("-------------------------")
+print("RESULTS")
+print("-------------------------")
+
+print("answers: ", end="")
+for x in answers:
+    print(x, end=" ")
+print()
+
+print("guesses: ", end="")
+for x in guesses:
+    print(x, end=" ")
+print()
+
+score = int(score / len(questions) * 100)
+print(f"Your score is: {score}%")
+
+if score >= 50:
+    print("You passed the quiz")
+else:
+    print("You failed the quiz")
+"""
